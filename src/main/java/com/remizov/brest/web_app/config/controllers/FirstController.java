93d8c113e405f50e50f0fa2fbb8685c3119ac2f3
Alex Remizov <rem.remizov@yandex.ru>
2021-12-23 00:37:41 -0500

    Add Controller intro

---
+++ src/main/java/com/remizov/brest/web_app/config/controllers/FirstController.java
@@ -0,0 +1,18 @@
+package com.remizov.brest.web_app.config.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/say")
+public class FirstController {
+    @GetMapping("/hello")
+    public String helloPage(){
+        return("first/hello");
+    }
+    @GetMapping ("/goodbye")
+    public String goodByePage(){
+        return("first/goodbye");
+    }
+}
