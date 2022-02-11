d8e63bf4bca35c993edee2a4d6fd98d92981ea5e
Alex Remizov <rem.remizov@yandex.ru>
2022-02-10 19:01:09 -0500

    Lesson27.NamedJdbcTemplate

--- src/main/java/com/remizov/brest/web_app/config/SpringConfig.java
+++ src/main/java/com/remizov/brest/web_app/config/SpringConfig.java
@@ -5,12 +5,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.remizov.brest.web_app")
@@ -45,5 +49,19 @@ public class SpringConfig implements WebMvcConfigurer {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
+    }
+    @Bean
+    DataSource dataSource (){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/first_db");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("postgre");
+
+        return dataSource;
+    }
+    @Bean
+     public NamedParameterJdbcTemplate jdbcTemplate(){
+        return new NamedParameterJdbcTemplate(dataSource());
     }
 }
--- src/main/java/com/remizov/brest/web_app/dao/PersonDao.java
+++ src/main/java/com/remizov/brest/web_app/dao/PersonDao.java
@@ -1,124 +1,54 @@
 package com.remizov.brest.web_app.dao;
 
 import com.remizov.brest.web_app.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Component;
 
-import java.sql.*;
-import java.util.ArrayList;
+
 import java.util.List;
 
 @Component
 public class PersonDao {
-    private static int PEOPLE_COUNT;
-    private static Connection connection;
-    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
-    private static final String USERNAME ="postgres";
-    private static final String PASSWORD ="postgre";
 
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-    private static final String SQL_GET_ALL_PERSON="SELECT * FROM person";
-
-
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    @Autowired
+    public PersonDao(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 
     public List<Person> index()  {
-        List<Person> people = new ArrayList<>();
-
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(SQL_GET_ALL_PERSON);
-
-            while (resultSet.next()){
-                Person person = new Person();
-                person.setId(resultSet.getInt("id"));
-                person.setName(resultSet.getString("name"));
-                person.setAge(resultSet.getInt("age"));
-                person.setEmail(resultSet.getString("email"));
-
-                people.add(person);
-                System.out.println(person);
-
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return people;
+        return namedParameterJdbcTemplate.query("SELECT * FROM person",new PersonMapper());
     }
-
     public Person show(int id) {
-        Person person = null;
-
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("SELECT * FROM person WHERE id=?");
-            preparedStatement.setInt(1,id);
-           ResultSet resultSet = preparedStatement.executeQuery();
-           resultSet.next();
-           person = new Person();
-           person.setId(resultSet.getInt("id"));
-           person.setName(resultSet.getString("name"));
-           person.setAge(resultSet.getInt("age"));
-           person.setEmail(resultSet.getString("email"));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return person;
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource("id", id);
+        return namedParameterJdbcTemplate.query("SELECT * FROM Person WHERE id= :id", sqlParameterSource,
+                        new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
     }
 
     public void save(Person person) {
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("INSERT INTO person VALUES(1, ?, ?, ?)");
-            preparedStatement.setString(1, person.getName());
-            preparedStatement.setInt(2, person.getAge());
-            preparedStatement.setString(3,person.getEmail());
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource("name", person.getName())
+                .addValue("age", person.getAge())
+                .addValue("email", person.getEmail());
+        namedParameterJdbcTemplate.update("INSERT INTO person(id ,name , age , email) VALUES(1, :name, :age, :email)",sqlParameterSource);
     }
 
     public void update(int id, Person updatedPerson) {
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("UPDATE  person SET name=?, age=?, email=? WHERE id=?");
-            preparedStatement.setString(1, updatedPerson.getName());
-            preparedStatement.setInt(2, updatedPerson.getAge());
-            preparedStatement.setString(3,updatedPerson.getEmail());
-            preparedStatement.setInt(4,id);
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource("name", updatedPerson.getName())
+                .addValue("age", updatedPerson.getAge())
+                .addValue("email", updatedPerson.getEmail());
 
-           preparedStatement.executeUpdate();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-
+         namedParameterJdbcTemplate.update("UPDATE person set name = :name, age = :age,email = :email WHERE id= :id", sqlParameterSource);
     }
 
     public void delete(int id) {
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("DELETE FROM person WHERE id=?");
-        preparedStatement.setInt(1,id);
-
-        preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource("id", id);
+        namedParameterJdbcTemplate.update("DELETE FROM person WHERE id= :id", sqlParameterSource);
     }
 }
 
